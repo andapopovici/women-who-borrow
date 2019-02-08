360420ac3245700d2cac8a0b588c265c4fd4a3ca
Ewa Lipinska <ewa.lipinska@hotmail.com>
2019-02-08 22:46:32 +0000

    List books on user's show page and remove confusing Back links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  resources :books
-  resources :tags
-  resources :users
-  root 'books#index'
+	resources :books
+
+	resources :tags
+
+	resources :users do 
+		collection do
+			get :search
+		end
+	end
+
+	root 'books#index'
 end
