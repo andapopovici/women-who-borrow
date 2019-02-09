e0b972b89e824c2a73881f026af1f770ff6cc40d
Ewa Lipinska <ewa.lipinska@hotmail.com>
2019-02-09 16:15:36 +0000

    Continue working on reserving functionality
    This commit makes it possible to withdraw a book reservation. It also moves the reserve/unreserve actions from Book to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
 
   has_many :books, dependent: :destroy
   has_many :reservations
+
+  def reserved_books
+  	reservations.map(&:book)
+  end
+
+  def reserve(book)
+    if book.update_attributes(status: Book::RESERVED)
+      reservation = Reservation.new(user: self, book: book)
+      reservation.save
+    end
+  end
+
+  def unreserve(book)
+    if book.reservation && book.update_attributes(status: Book::FREE)
+      book.reservation.destroy
+    end
+  end
 end
