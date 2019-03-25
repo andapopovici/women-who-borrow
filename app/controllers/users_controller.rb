a1a427c37eabb3dea916253ff20ce83e589e6656
Ewa <ewa.lipinska@hotmail.com>
2019-03-25 15:49:02 +0000

    Remove unnecessary private tag

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,8 +37,6 @@ class UsersController < Clearance::UsersController
         @user = User.find(params[:id])
     end
 
-    private
-
     def user_params
         params[:user].permit(:email, :password, :first_name, :last_name)
     end
